f123f8a72dccc938a2d10f3b27e1901c46fe9da2
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-13 18:12:39 +0700

    резолв конфликта

--- src/prime-preset/map-tokens.ts
+++ src/prime-preset/map-tokens.ts
@@ -4,11 +4,8 @@ import type { AuraBaseDesignTokens } from '@primeuix/themes/aura/base';
 
 import tokens from './tokens/tokens.json';
 import { buttonCss } from './tokens/components/button';
-<<<<<<< overlay.dialog
 import { dialogCss } from './tokens/components/dialog';
-=======
 import { radiobuttonCss } from './tokens/components/radiobutton';
->>>>>>> feature/styles-debug
 
 const presetTokens: Preset<AuraBaseDesignTokens> = {
   primitive: tokens.primitive as unknown as AuraBaseDesignTokens['primitive'],
@@ -19,15 +16,13 @@ const presetTokens: Preset<AuraBaseDesignTokens> = {
       ...(tokens.components.button as unknown as ComponentsDesignTokens['button']),
       css: buttonCss,
     },
-<<<<<<< overlay.dialog
     dialog: {
       ...(tokens.components.dialog as unknown as ComponentsDesignTokens['dialog']),
       css: dialogCss,
-=======
+    },
     radiobutton: {
       ...(tokens.components.radiobutton as unknown as ComponentsDesignTokens['radiobutton']),
       css: radiobuttonCss,
->>>>>>> feature/styles-debug
     },
   } as ComponentsDesignTokens,
 };
