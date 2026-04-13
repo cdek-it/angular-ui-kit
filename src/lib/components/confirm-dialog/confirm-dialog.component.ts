1627b925a23ac8cfd172ccca60043aa52fd157f4
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-13 18:28:50 +0700

    confirm-dialog: добавлены headerTemplate и footerTemplate для кастомизации

--- src/lib/components/confirm-dialog/confirm-dialog.component.ts
+++ src/lib/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { ConfirmDialog } from 'primeng/confirmdialog';
 import { Button } from 'primeng/button';
 import { PrimeTemplate } from 'primeng/api';
@@ -10,39 +11,51 @@ export type ConfirmDialogSeverity = 'success' | 'info' | 'warn' | 'help' | 'dang
   selector: 'confirm-dialog',
   host: { style: 'display: contents' },
   standalone: true,
-  imports: [ConfirmDialog, Button, PrimeTemplate],
+  imports: [ConfirmDialog, Button, PrimeTemplate, NgTemplateOutlet],
   template: `
     <p-confirmDialog [key]="key" [styleClass]="computedClass" appendTo="body">
       <ng-template pTemplate="headless" let-message let-onAccept="onAccept" let-onReject="onReject">
-        <div class="p-dialog-header">
-          <div class="p-dialog-title">
-            <i [class]="message.icon"></i>
-            <span>{{ message.header }}</span>
+        @if (headerTemplate) {
+          <ng-container [ngTemplateOutlet]="headerTemplate"
+            [ngTemplateOutletContext]="{ $implicit: message, onAccept, onReject }">
+          </ng-container>
+        } @else {
+          <div class="p-dialog-header">
+            <div class="p-dialog-title">
+              <i [class]="message.icon"></i>
+              <span>{{ message.header }}</span>
+            </div>
+            <button
+              class="p-button p-component p-button-icon-only p-button-rounded p-button-text p-dialog-close-button"
+              type="button"
+              (click)="onReject()"
+            >
+              <span class="p-button-icon ti ti-x"></span>
+            </button>
           </div>
-          <button
-            class="p-button p-component p-button-icon-only p-button-rounded p-button-text p-dialog-close-button"
-            type="button"
-            (click)="onReject()"
-          >
-            <span class="p-button-icon ti ti-x"></span>
-          </button>
-        </div>
+        }
         <div class="p-dialog-content">
           <p>{{ message.message }}</p>
         </div>
-        <div class="p-dialog-footer">
-          <p-button
-            [label]="message.rejectLabel"
-            severity="secondary"
-            variant="text"
-            (onClick)="onReject()"
-          ></p-button>
-          <p-button
-            [label]="message.acceptLabel"
-            [severity]="message.acceptButtonProps?.severity"
-            (onClick)="onAccept()"
-          ></p-button>
-        </div>
+        @if (footerTemplate) {
+          <ng-container [ngTemplateOutlet]="footerTemplate"
+            [ngTemplateOutletContext]="{ $implicit: message, onAccept, onReject }">
+          </ng-container>
+        } @else {
+          <div class="p-dialog-footer">
+            <p-button
+              [label]="message.rejectLabel"
+              severity="secondary"
+              variant="text"
+              (onClick)="onReject()"
+            ></p-button>
+            <p-button
+              [label]="message.acceptLabel"
+              [severity]="message.acceptButtonProps?.severity"
+              (onClick)="onAccept()"
+            ></p-button>
+          </div>
+        }
       </ng-template>
     </p-confirmDialog>
   `,
@@ -51,6 +64,8 @@ export class ConfirmDialogComponent {
   @Input() key = '';
   @Input() size: ConfirmDialogSize = 'default';
   @Input() severity: ConfirmDialogSeverity = 'default';
+  @Input() headerTemplate: TemplateRef<any> | null = null;
+  @Input() footerTemplate: TemplateRef<any> | null = null;
 
   get computedClass(): string {
     const classes: string[] = [];
