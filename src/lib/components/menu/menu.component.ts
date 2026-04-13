7896268e4d8dc53bae61073173fa24d71061a510
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-13 18:55:47 +0700

    menu: добавлен itemTemplate для кастомизации элемента меню

--- src/lib/components/menu/menu.component.ts
+++ src/lib/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { Menu } from 'primeng/menu';
 import { MenuItem, PrimeTemplate } from 'primeng/api';
 
@@ -10,31 +11,37 @@ export interface MenuModel extends MenuItem {
   selector: 'menu',
   host: { style: 'display: contents' },
   standalone: true,
-  imports: [Menu, PrimeTemplate],
+  imports: [Menu, PrimeTemplate, NgTemplateOutlet],
   template: `
     <p-menu #menuRef [model]="model" [popup]="popup" [appendTo]="popup ? 'body' : null">
       <ng-template pTemplate="item" let-item>
-        <a
-          class="p-menu-item-link"
-          role="menuitem"
-          tabindex="0"
-          [class.p-disabled]="item.disabled"
-          [attr.href]="item.url || null"
-          [attr.target]="item.target || null"
-          (click)="!item.disabled && item.command && item.command({ originalEvent: $event, item: item })"
-        >
-          @if (item.icon) {
-            <span [class]="item.icon + ' p-menu-item-icon'"></span>
-          }
-          @if ($any(item).caption) {
-            <div class="menu-item-label">
+        @if (itemTemplate) {
+          <ng-container [ngTemplateOutlet]="itemTemplate"
+            [ngTemplateOutletContext]="{ $implicit: item }">
+          </ng-container>
+        } @else {
+          <a
+            class="p-menu-item-link"
+            role="menuitem"
+            tabindex="0"
+            [class.p-disabled]="item.disabled"
+            [attr.href]="item.url || null"
+            [attr.target]="item.target || null"
+            (click)="!item.disabled && item.command && item.command({ originalEvent: $event, item: item })"
+          >
+            @if (item.icon) {
+              <span [class]="item.icon + ' p-menu-item-icon'"></span>
+            }
+            @if ($any(item).caption) {
+              <div class="menu-item-label">
+                <span class="p-menu-item-label">{{ item.label }}</span>
+                <small class="menu-item-caption">{{ $any(item).caption }}</small>
+              </div>
+            } @else {
               <span class="p-menu-item-label">{{ item.label }}</span>
-              <small class="menu-item-caption">{{ $any(item).caption }}</small>
-            </div>
-          } @else {
-            <span class="p-menu-item-label">{{ item.label }}</span>
-          }
-        </a>
+            }
+          </a>
+        }
       </ng-template>
     </p-menu>
   `,
@@ -44,6 +51,7 @@ export class MenuComponent {
 
   @Input() model: MenuModel[] = [];
   @Input() popup = false;
+  @Input() itemTemplate: TemplateRef<any> | null = null;
 
   toggle(event: Event): void {
     this.menuRef.toggle(event);
