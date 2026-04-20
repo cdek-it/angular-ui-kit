318544bfbf394bf8daf7716ff4d0e3ee9ddcd794
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-20 10:17:53 +0700

    fix(menubar): удалить сторис Default

--- src/stories/components/menubar/menubar.stories.ts
+++ src/stories/components/menubar/menubar.stories.ts
@@ -1,10 +1,8 @@
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, moduleMetadata } from '@storybook/angular';
 import { MenubarComponent } from '../../../lib/components/menubar/menubar.component';
 import { MenubarBasicComponent, Basic } from './examples/menubar-basic.component';
 import { MenubarWithIconComponent, WithIcon } from './examples/menubar-with-icon.component';
 import { MenubarCustomComponent, Custom } from './examples/menubar-custom.component';
-import { basicItems } from './menubar.data';
-
 const meta: Meta<MenubarComponent> = {
   title: 'Components/Menu/Menubar',
   component: MenubarComponent,
@@ -35,21 +33,5 @@ import { MenubarComponent } from '@cdek-it/angular-ui-kit';
 };
 
 export default meta;
-type Story = StoryObj<MenubarComponent>;
-
-export const Default: Story = {
-  name: 'Default',
-  render: (args) => ({
-    props: { ...args, model: basicItems },
-    template: `<menubar [model]="model"></menubar>`,
-  }),
-  parameters: {
-    docs: {
-      description: {
-        story: 'Базовый пример компонента.',
-      },
-    },
-  },
-};
 
 export { Basic, WithIcon, Custom };
