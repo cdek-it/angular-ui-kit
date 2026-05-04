2925824f137546327206fc5511d8b7fd3d6edb53
Игорь Чиёсов <i.chiesov@gmail.com>
2026-05-04 15:31:28 +0700

    ~fix(input-otp): add missing import.

--- src/prime-preset/map-tokens.ts
+++ src/prime-preset/map-tokens.ts
@@ -16,6 +16,7 @@ import { tooltipCss } from './tokens/components/tooltip';
 import { megamenuCss } from './tokens/components/megamenu';
 import { selectCss } from './tokens/components/select';
 import { messageCss } from './tokens/components/message';
+import { inputotpCss } from './tokens/components/inputotp';
 
 const presetTokens: Preset<AuraBaseDesignTokens> = {
   primitive: tokens.primitive as unknown as AuraBaseDesignTokens['primitive'],
