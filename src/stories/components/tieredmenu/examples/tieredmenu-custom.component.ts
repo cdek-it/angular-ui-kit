c3f67bb0241dce8ede9abafd2b3cbcf4edbdc003
ak.dmitriev <ak.dmitriev@cdek.ru>
2026-06-18 16:05:02 +0700

    fix

--- src/stories/components/tieredmenu/examples/tieredmenu-custom.component.ts
+++ src/stories/components/tieredmenu/examples/tieredmenu-custom.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { StoryObj } from '@storybook/angular';
-import { ExtraMenuItem } from '../../../../lib/shared';
+import { ExtraMenuItem } from '../../../../lib/shared/public_api';
 import { Badge } from 'primeng/badge';
 import { ExtraTieredMenuComponent } from '../../../../lib/components/tieredmenu/tieredmenu.component';
 
