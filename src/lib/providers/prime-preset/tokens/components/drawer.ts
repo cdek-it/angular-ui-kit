4cd9557b1ae5c1b9e68b4fddbc4fd87f544ea6ca
Danil Khaliulin <danielooneilie@gmail.com>
2026-08-28 19:10:27 +0800

    задать ширину drawer по размерам: sm 280, base 350, lg 420, xlg 630

--- src/lib/providers/prime-preset/tokens/components/drawer.ts
+++ src/lib/providers/prime-preset/tokens/components/drawer.ts
@@ -29,11 +29,28 @@ export const drawerCss = ({ dt }: { dt: (token: string) => string }): string => 
   padding: 0 ${dt('dimension.space.600')} ${dt('dimension.space.600')} ${dt('dimension.space.600')};
 }
 
+/* Ширина панели: в экспорте у drawer одна ширина, поэтому размеры берём с той же ступени
+   примитивов, что рисует Figma (коллекция sizing, width: 280 / 350 / 420 / 630) */
 .p-drawer.p-component.p-drawer-left,
 .p-drawer.p-component.p-drawer-right {
   margin: ${dt('drawer.extend.margin')};
-  width: ${dt('drawer.extend.width')};
+  width: ${dt('sizing.175x')};
   height: calc(100% - ${dt('drawer.extend.margin')} * 2);
+}
+
+.p-drawer.p-component.p-drawer-left.p-drawer-sm,
+.p-drawer.p-component.p-drawer-right.p-drawer-sm {
+  width: ${dt('sizing.140x')};
+}
+
+.p-drawer.p-component.p-drawer-left.p-drawer-lg,
+.p-drawer.p-component.p-drawer-right.p-drawer-lg {
+  width: ${dt('sizing.210x')};
+}
+
+.p-drawer.p-component.p-drawer-left.p-drawer-xlg,
+.p-drawer.p-component.p-drawer-right.p-drawer-xlg {
+  width: ${dt('sizing.315x')};
 }
 
 .p-drawer.p-component.p-drawer-top,
