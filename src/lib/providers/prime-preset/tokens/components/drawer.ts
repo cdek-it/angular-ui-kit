d0458f6273e6ced59c05b74ab30c65b004033927
Danil Khaliulin <danielooneilie@gmail.com>
2026-08-28 19:48:30 +0800

    привязать ширины drawer к семантической шкале размеров и добавить ступень 630

--- src/lib/providers/prime-preset/tokens/components/drawer.ts
+++ src/lib/providers/prime-preset/tokens/components/drawer.ts
@@ -29,28 +29,28 @@ export const drawerCss = ({ dt }: { dt: (token: string) => string }): string => 
   padding: 0 ${dt('dimension.space.600')} ${dt('dimension.space.600')} ${dt('dimension.space.600')};
 }
 
-/* Ширина панели: base приходит токеном компонента, а для sm/lg/xlg ветки в экспорте нет —
-   значения берём с той же ступени примитивов, что рисует Figma (sizing, width: 280 / 420 / 630) */
+/* Ширина панели по размерам: drawer.extend.overlayWidth — ступени семантической шкалы
+   dimension.size (макет просит 280 / 350 / 420 / 630, ближайшие ступени дают 272 / 336 / 400 / 630) */
 .p-drawer.p-component.p-drawer-left,
 .p-drawer.p-component.p-drawer-right {
   margin: ${dt('drawer.extend.margin')};
-  width: ${dt('drawer.extend.width')};
+  width: ${dt('drawer.extend.overlayWidth.base')};
   height: calc(100% - ${dt('drawer.extend.margin')} * 2);
 }
 
 .p-drawer.p-component.p-drawer-left.p-drawer-sm,
 .p-drawer.p-component.p-drawer-right.p-drawer-sm {
-  width: ${dt('sizing.140x')};
+  width: ${dt('drawer.extend.overlayWidth.sm')};
 }
 
 .p-drawer.p-component.p-drawer-left.p-drawer-lg,
 .p-drawer.p-component.p-drawer-right.p-drawer-lg {
-  width: ${dt('sizing.210x')};
+  width: ${dt('drawer.extend.overlayWidth.lg')};
 }
 
 .p-drawer.p-component.p-drawer-left.p-drawer-xlg,
 .p-drawer.p-component.p-drawer-right.p-drawer-xlg {
-  width: ${dt('sizing.315x')};
+  width: ${dt('drawer.extend.overlayWidth.xlg')};
 }
 
 .p-drawer.p-component.p-drawer-top,
